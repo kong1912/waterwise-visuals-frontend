c68fb6c8138d937afaee0c64b0372a6172b9bc58
Beammm2134 <beamatipat@gmail.com>
2024-12-01 15:50:46 +0700

    Update WateringControl.tsx

--- src/components/WateringControl.tsx
+++ src/components/WateringControl.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 
 const WateringControl = () => {
-  const [imageSrc, setImageSrc] = useState("http://192.168.43.18/custom");
+  const [imageSrc, setImageSrc] = useState("https://manatee-steady-immensely.ngrok-free.app/camera");
 
   useEffect(() => {
     const updateImage = () => {
       const timestamp = new Date().getTime(); // Add a timestamp to avoid caching
-      setImageSrc(`http://192.168.43.18/custom?timestamp=${timestamp}`);
+      setImageSrc(`https://manatee-steady-immensely.ngrok-free.app/camera?timestamp=${timestamp}`);
     };
 
     updateImage(); // Update immediately on mount
